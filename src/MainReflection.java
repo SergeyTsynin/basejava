6e728ca7e9723d83b8f6ebe3338385880146296c
SergeyTsinin <sergey.tsynin@gmail.com>
2020-01-13 13:57:27 +0300

    HW4 TODO & trash removed

--- src/MainReflection.java
+++ src/MainReflection.java
@@ -1,7 +1,4 @@
 import model.Resume;
-import storage.ArrayStorage;
-import storage.Storage;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -18,14 +15,7 @@ public class MainReflection {
         field.set(r, "new uuid");
         System.out.println(r);
 
-        // TODO : invoke r.toString via reflection 13:00
         Method method = r.getClass().getDeclaredMethod("toString");
         System.out.println(method.invoke(r));
-
-        Storage storage = new ArrayStorage();
-        storage.save(new Resume("uuid1"));
-        storage.save(new Resume("uuid2"));
-        storage.save(new Resume("uuid3"));
-        System.out.println(storage.size());
     }
 }
