baa4f6c4e38a37ecbac4d2f9eb0f6a7d690e82de
SergeyTsinin <sergey.tsynin@gmail.com>
2020-05-18 22:13:44 +0300

    HW7 create enum ContactType

---
+++ src/model/ContactType.java
@@ -0,0 +1,21 @@
+package model;
+
+public enum ContactType {
+    PHONE("Тел."),
+    SKYPE("Skype"),
+    EMAIL("Почта"),
+    LINKEDIN("LinkedIn"),
+    GITHUB("GitHub"),
+    STACKOVERFLOW("Stackoverflow"),
+    HOMEPAGE("Домашняя страница");
+
+    private final String title;
+
+    ContactType(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
