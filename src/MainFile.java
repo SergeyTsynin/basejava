332a4b4cb5cd947e501b2a684c71418563bce1e8
SergeyTsinin <sergey.tsynin@gmail.com>
2020-06-04 16:53:19 +0300

    HW8 рекурсию будет делать проще по файлу, а не по его пути, доставай вместо списка имен файлов, список файлов в директории

--- src/MainFile.java
+++ src/MainFile.java
@@ -2,18 +2,20 @@ import java.io.File;
 
 public class MainFile {
     public static void main(String[] args) {
-        trackMan(".\\src");
+        File folder = new File(".\\src");
+        trackMan2(folder);
     }
 
-    private static void trackMan(String dir) {
-        File folder = new File(dir);
-        if (folder.isFile()) {
-            String fileName = folder.getName();
-            System.out.println(fileName.substring(0, fileName.lastIndexOf(".")));
-        } else {
-            if (!folder.exists()) return;
-            for (String name : folder.list()) {
-                trackMan(folder + "\\" + name);
+    private static void trackMan2(File file) {
+        File[] pig = file.listFiles();
+        if (pig != null) {
+            for (File value : pig) {
+                if (value.isDirectory()) {
+                    trackMan2(value);
+                } else {
+                    String name = value.getName();
+                    System.out.println(name.substring(0, name.lastIndexOf(".")));
+                }
             }
         }
     }
