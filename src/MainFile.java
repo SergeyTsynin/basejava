bdfbfc5bdcefd7f23caa829441a70c24f2890e9d
SergeyTsinin <sergey.tsynin@gmail.com>
2020-06-12 19:32:32 +0300

    HW9 tabulated recursive output of directories and files

--- src/MainFile.java
+++ src/MainFile.java
@@ -3,7 +3,8 @@ import java.io.File;
 public class MainFile {
     public static void main(String[] args) {
         File folder = new File(".\\src");
-        trackMan2(folder);
+//        trackMan2(folder);
+        trackMan3(folder, "");
     }
 
     private static void trackMan2(File file) {
@@ -15,6 +16,20 @@ public class MainFile {
                 } else {
                     String name = value.getName();
                     System.out.println(name.substring(0, name.lastIndexOf(".")));
+                }
+            }
+        }
+    }
+
+    private static void trackMan3(File file, String tab) {
+        File[] pig = file.listFiles();
+        if (pig != null) {
+            for (File value : pig) {
+                if (value.isDirectory()) {
+                    System.out.println(tab + " + " + value.getName());
+                    trackMan3(value, " " + tab);
+                } else {
+                    System.out.println(tab + "| " + value.getName());
                 }
             }
         }
