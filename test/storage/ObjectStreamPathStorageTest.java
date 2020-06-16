81083cff2ca502677d09b58a9f8e9022ce2fa7a5
SergeyTsinin <sergey.tsynin@gmail.com>
2020-06-16 21:00:18 +0300

    HW9 change input parameter (File > String)2

--- test/storage/ObjectStreamPathStorageTest.java
+++ test/storage/ObjectStreamPathStorageTest.java
@@ -1,10 +1,8 @@
 package storage;
 
-import java.io.File;
-
 public class ObjectStreamPathStorageTest extends AbstractStorageTest {
 
     public ObjectStreamPathStorageTest() {
-        super(new ObjectStreamPathStorage(new File(STORAGE_DIR)));
+        super(new ObjectStreamPathStorage(STORAGE_DIR));
     }
 }
